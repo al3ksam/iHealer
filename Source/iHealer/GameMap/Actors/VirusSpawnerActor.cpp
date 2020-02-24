2aba239509e72cb870516c7cd2f384a7ff927c7c
al3ksam <al3ksam@ya.ru>
2020-02-24 21:59:55 +0300

    #Update_5

---
+++ Source/iHealer/GameMap/Actors/VirusSpawnerActor.cpp
@@ -0,0 +1,27 @@
+// Copyright 1998-2020 Epic Games, Inc. All Rights Reserved.
+
+
+#include "VirusSpawnerActor.h"
+
+// Sets default values
+AVirusSpawnerActor::AVirusSpawnerActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AVirusSpawnerActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVirusSpawnerActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
