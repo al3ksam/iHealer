fcb8f832e9146684a81702402ea71f90a506b0a4
al3ksam <al3ksam@ya.ru>
2020-02-25 00:05:13 +0300

    #Update_10

--- Build/Android/src/com/deralexam/ihealer/OBBData.java
+++ Build/Android/src/com/deralexam/ihealer/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-29300469L // the length of the file in bytes
+30296686L // the length of the file in bytes
 )
 };
 };
