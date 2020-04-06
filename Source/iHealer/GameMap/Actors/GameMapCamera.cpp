5e6554e432e84970d78389fb2d16181920e44b94
al3ksam <al3ksam@ya.ru>
2020-04-07 00:44:10 +0300

    #Update_2020_04_07

--- Source/iHealer/GameMap/Actors/GameMapCamera.cpp
+++ Source/iHealer/GameMap/Actors/GameMapCamera.cpp
@@ -2,15 +2,12 @@
 
 #include "GameMapCamera.h"
 #include "Camera/CameraComponent.h"
-#include "Kismet/GameplayStatics.h"
 
 AGameMapCamera::AGameMapCamera()
 {
-	// Set this actor to call Tick() every frame
-	PrimaryActorTick.bCanEverTick = false;
+	UCameraComponent* GameMapCameraComponent = this->GetCameraComponent();
 
 	// Setting the camera component for an orthogonal view 
-	UCameraComponent* GameMapCameraComponent = this->GetCameraComponent();
 	GameMapCameraComponent->ProjectionMode = ECameraProjectionMode::Orthographic;
 	GameMapCameraComponent->OrthoWidth = 1024.0f;
 	GameMapCameraComponent->bConstrainAspectRatio = false;
