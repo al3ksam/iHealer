e8eb5524114fd3c8b0db5b732418a7ec18bc681f
al3ksam <al3ksam@ya.ru>
2020-04-11 04:01:30 +0300

    #Update_2020_04_11

---
+++ Source/iHealer/GameMap/Controllers/GameMapVirusAIController.cpp
@@ -0,0 +1,5 @@
+// Copyright 1998-2020 Epic Games, Inc. All Rights Reserved.
+
+
+#include "GameMapVirusAIController.h"
+
---
+++ Source/iHealer/GameMap/Controllers/GameMapVirusAIController.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2020 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "GameMapVirusAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IHEALER_API AGameMapVirusAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
